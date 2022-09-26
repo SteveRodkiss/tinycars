2332fe57a38de9e21be3302080d370ba818ac1e7
SteveRodkiss <steve.rodkiss@gmail.com>
2022-09-26 16:08:39 +1300

    added a couple of modls and a test level

--- Scripts/car_ai_steerer.gd
+++ Scripts/car_ai_steerer.gd
@@ -16,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	car.steer_input = ai_input.x
 	car.throttle_input = ai_input.y
 
+
 func get_ai_input(target : Vector3) -> Vector2:
 	target_position = _target.global_translation
 	var direction := Vector2.ZERO
@@ -27,15 +28,15 @@ func get_ai_input(target : Vector3) -> Vector2:
 	#now work out if we want to back backwards or forward
 	var throttle_relative = direction_relative.dot(-global_transform.basis.z)
 	if throttle_relative > 0.0:
-		print("go go go")
+		#print("go go go")
 		direction.y = throttle_relative
 	else:
 		#slow down to turn around!!
 		if car.get_velocity() > 300:
-			print("brake!", car.get_velocity())
+			#print("brake!", car.get_velocity())
 			#brake
 			direction.y = -1.0
 		else:
-			print("slow")
+			#print("slow")
 			direction.y = 0.05
 	return direction
